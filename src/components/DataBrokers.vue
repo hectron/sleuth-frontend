6fdc3f535a020f7e30b9f132af026bb18645164a
Hector Rios <that.hector@gmail.com>
2022-07-03 15:49:20 -0500

    init: Initial commit

---
+++ src/components/DataBrokers.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from "axios";
+
+const requestBrokers = () => {
+  let brokers = []
+
+  axios
+    .get("http://localhost:3000/data_brokers")
+    .then(response => {
+      console.log(response)
+      brokers = response.data.brokers
+    })
+    .catch(err => console.log(err))
+    .then(() => console.log("Done"))
+
+  return brokers
+}
+
+let brokers = await requestBrokers()
+</script>
+
+<template>
+  <li v-for="broker in brokers">
+    {{ broker.name }}
+  </li>
+</template>
